6166691467860f5374e6b407eafeece8d61edbcc
Chaehyun Lee <cogusdl1113@gmail.com>
2021-05-30 23:20:48 +0900

    첫번째 화면

---
+++ app/src/main/java/com/termproject/weatherwear/SecondActivity.java
@@ -0,0 +1,30 @@
+package com.termproject.weatherwear;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+
+    private TextView tCity;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.secondactivity);
+
+        tCity = findViewById(R.id.tCity);
+
+        Intent intent = getIntent();
+        String str = intent.getStringExtra("city");
+        //String strUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + str + "&appid=867d2553fb743f162631454d6ebde974&lang=kr";
+
+        tCity.setText(str);
+    }
+
+}
